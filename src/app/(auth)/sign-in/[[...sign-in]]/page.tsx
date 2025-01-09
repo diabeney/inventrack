16921118bf6b573b9513cbb7ef030c1bf204803e
Diabene <addodiabene69@gmail.com>
2025-01-09 12:46:37 +0000

    feat(app): fix

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className=" grid place-content-center h-screen">
-      <SignIn />;
+      <SignIn />
     </div>
   );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className=" grid place-content-center h-screen">
-      <SignUp />;
+      <SignUp />
     </div>
   );
 }
